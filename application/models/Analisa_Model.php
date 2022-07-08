59d80a0df7eb5085ad6c6dd74b23784f3fef06d7
andikkurniawan <andikk362@gmail.com>
2022-07-08 09:07:35 +0700

    add model for analisa and report

---
+++ application/models/Analisa_Model.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Analisa_Model extends CI_Model {
+
+    
+
+}
---
+++ application/models/Report_Model.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Report_Model extends CI_Model {
+
+    
+
+}
