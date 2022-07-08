3e52019e1dd09df82186294a64824e2f615f1580
andikkurniawan <andikk362@gmail.com>
2022-07-08 16:04:49 +0700

    make view for penguapan

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -57,13 +57,30 @@ class Welcome extends CI_Controller {
 				$data['id_gilingan']		= $this->ID_Sampel_Model->getIDForGilingan();
 				$data['id_ampas_gilingan']	= $this->ID_Sampel_Model->getIDForAmpasGilingan();
 				$data['npp']				= $this->Analisa_Model->getAnalisaNppLatest5();
-				for($i=0; $i < count($data['id_gilingan']); $i++) $data['nira_gilingan'][$i+2] = $this->Analisa_Model->getAnalisaBrixPolLatest5($data['id_gilingan'][$i]);
-				for($i=0; $i < count($data['id_ampas_gilingan']); $i++) $data['ampas_gilingan'][$i+1] = $this->Analisa_Model->getAnalisaAmpasLatest5($data['id_ampas_gilingan'][$i]);
+
+				for($i=0; $i < count($data['id_gilingan']); $i++) 
+					$data['nira_gilingan'][$i+2] = $this->Analisa_Model->getAnalisaBrixPolLatest5($data['id_gilingan'][$i]);
+
+				for($i=0; $i < count($data['id_ampas_gilingan']); $i++) 
+					$data['ampas_gilingan'][$i+1] = $this->Analisa_Model->getAnalisaAmpasLatest5($data['id_ampas_gilingan'][$i]);
 			break;
 
 			case 'pemurnian' :
 				$data['id_nira_pemurnian']	= $this->ID_Sampel_Model->getIDForNiraPemurnian();
-				for($i=0; $i < count($data['id_nira_pemurnian']); $i++)$data['nira_pemurnian'][$i] = $this->Analisa_Model->getAnalisaBrixPolLatest5($data['id_nira_pemurnian'][$i]);
+				$data['id_blotong']			= $this->ID_Sampel_Model->getIDForBlotong();
+
+				for($i=0; $i < count($data['id_nira_pemurnian']); $i++)
+					$data['nira_pemurnian'][$i] = $this->Analisa_Model->getAnalisaBrixPolLatest5($data['id_nira_pemurnian'][$i]);
+
+				for($i=0; $i < count($data['id_blotong']); $i++) 
+					$data['blotong'][$i] = $this->Analisa_Model->getAnalisaBlotongLatest5($data['id_blotong'][$i]);
+			break;
+
+			case 'penguapan' :
+				$data['id_penguapan']		= $this->ID_Sampel_Model->getIDForPenguapan();
+
+				for($i=0; $i < count($data['id_penguapan']); $i++)
+					$data['penguapan'][$i] = $this->Analisa_Model->getAnalisaBrixPolLatest5($data['id_penguapan'][$i]);
 			break;
 		}
 
---
+++ application/views/analisa/penguapan.php
@@ -0,0 +1,46 @@
+
+          <div class="container-fluid">
+
+          <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="<?=base_url();?>">Home</a></li>
+                  <li class="breadcrumb-item"><a href="<?=base_url('welcome/analisa');?>">Analisa</a></li>
+                  <li class="breadcrumb-item active" aria-current="page"><?=$page_title;?></li>
+              </ol>
+          </nav>
+          <hr>
+
+        <div class="row">
+
+          <?php for($i=0; $i < count($penguapan); $i++): ?>
+
+              <div class="col-md-4">
+                              
+                  <h5>Evaporator <?=$i;?></h5>
+                      <table class="table table-sm table-bordered table-hover">
+
+                          <tr>
+                              <th>Time</th>
+                              <th>Brix</th>
+                          </tr>
+
+                          <?php foreach($penguapan[$i] as $penguapan[$i]): ?>
+                          <tr>
+                              <td><?=date('H:i', strtotime($penguapan[$i]->waktu));?></td>
+                              <td><?=number_format($penguapan[$i]->brix,2);?></td>
+                          </tr>
+                          <?php endforeach; ?>
+
+                      </table>
+                  <br>
+
+              </div>
+
+          <?php endfor; ?>
+
+        </div>
+
+    </div>
+
+  </section>
+  
