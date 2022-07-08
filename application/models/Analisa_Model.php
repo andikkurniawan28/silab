a78c4cde6f39d80b380c8291591a6252cb96e90f
andikkurniawan <andikk362@gmail.com>
2022-07-08 10:01:09 +0700

    add function getAnalisa

--- application/models/Analisa_Model.php
+++ application/models/Analisa_Model.php
@@ -3,9 +3,17 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Analisa_Model extends CI_Model {
 
-    public function getAnalisaBrixPol()
+    public function getAnalisaNppLatest5()
     {
-        
+        return $this->db->query("select * from `analisa_npp` order by `id` desc limit 0,5")->result();
+    }
+
+    public function getAnalisaBrixPolLatest5($id)
+    {
+        $max_id = ($id + 1) * 10000;
+        $min_id = $id * 10000;
+
+        return $this->db->query("select * from `saccharomat` where `bahan` between $min_id and $max_id order by `id` desc limit 0,5")->result();
     }
 
 }
