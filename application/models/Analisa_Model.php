80a30f5da91a30e74af4520028f95d4f849909e6
andikkurniawan <andikk362@gmail.com>
2022-07-08 09:11:54 +0700

    add function

--- application/models/Analisa_Model.php
+++ application/models/Analisa_Model.php
@@ -3,6 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Analisa_Model extends CI_Model {
 
-    
+    public function getAnalisaBrixPol()
+    {
+        
+    }
 
 }
